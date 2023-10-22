57b061997141d47e08221d01962b718531a60cdb
mahyarfardin <mahyarfardinfar@gmail.com>
2023-10-22 23:54:27 +0330

    first commit

---
+++ 1. Josephus Problem/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+  public static int Josephus(int N, int k) {
+    int i = 1, ans = 0;
+    while (i <= N) {
+      ans = (ans + k) % i;
+      i++;
+    }
+    return ans + 1;
+  }
+
+  static int josephus(int N, int K) {
+    if (N == 1)
+      return 1;
+    else
+      return (josephus(N - 1, K) + K - 1) % N + 1;
+  }
+
+  public static void main(String[] args) {
+    int N = 11, k = 4;
+    String[] names = { "Hasan", "Nima", "Reza", "Amir", "Zahra", "Deniz", "Ali", "Sana", "Mahyar", "Mammad", "Hooman" };
+    int ans1 = Josephus(N, k);
+    System.out.println("Lucky " + names[ans1] + " :)");
+    int ans2 = Josephus(N, k);
+    System.out.println("Lucky " + names[ans2] + " :)");
+  }
+}
